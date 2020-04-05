e8fd66f3e90697b94b37259b3eb2c8d4538a6640
EfDe2020 <63202021+EfDe2020@users.noreply.github.com>
2020-04-05 18:39:56 +0200

    Add files via upload

---
+++ run_main1.m
@@ -0,0 +1,226 @@
+close all; clearvars;
+
+global sigma gamma b alpha1 alpha2 Mort_Rate pC pN pR wC wR NC scenario scenario_p JDI JAI K Pos Posl N J T1 T0 Country I0 D0 H0 delay Type J_id;
+
+%% Initialization
+% Model parameters
+sigma = 1/7; % incubation rate [1/12 1/2]
+gamma = [0.0721 0.238]; % probability of recovery or death (for China)
+b = [0.05068 0.05429]; % probability of trasmsmission during a contact (for China)
+alpha1 = 5; alpha2 = alpha1; % ration between measured/unmeasured and/or between I and E, it is in [5 20]
+Mort_Rate = 0.034; % mortality rate is 3.4 (for China)
+delay = 10;
+
+% Confinement parameters
+pC = 2; % number of contacts in quarantin (3 for China)
+pN = 12; % number of contacts in normal state (15 for China)
+pR = 6; % number of contacts in relaxed quarantin (10 for China)
+
+% Scenario of quarantine 
+scenario = 1;
+if (scenario == 1)
+    wC = 6; % weeks in quarantine
+    wR = 2; % weeks in relaxed quarantine
+    NC = 3; % number cycles to simulate
+else
+    wC = 12; % weeks in quarantine
+    wR = 4; % weeks in relaxed quarantine
+    NC = 2; % number cycles to simulate
+end
+
+% Identification and prediciton
+delta = .1; % the level of uncertainty
+JDI = 5; JAI = 12; % days used for identification of alpha
+scenario_p = 1; % the algorithm for identification
+if (scenario_p == 1)
+    K = 6; % length of window to estimate the values of parameters
+else
+    K = 10; % length of window to estimate the values of parameters
+end
+J_id = 0; % the number of days used for the model verification
+
+% Misc
+Pos = [200 200 900 600];
+Posl = [200 200 1500 600];
+Type = 1; % Type of the plot
+Imperial = 2; % 1 = Use of Imperial College parameters; 2 = Use of Cmmdi parameters
+China = 0;
+
+%% France
+Country = 'France';
+
+% ARS statistics from 12/03
+I0 = [2876 3661 4499 5423 6633 7730 9134 10995 12612 14459 16018 19856 22302 25230 29155 32964 37575 40174 44550 52128 56989 59105 64338 68605]; % detected infected
+D0 = [61 79 91 127 148 175 244 372 450 562 674 860 1100 1331 1696 1995 2314 2606 3024 3523 4032 4503 5091 5532]; % detected deaths
+H0 = [12 12 12 12 12 12 12 12 12 12 2200 2200 3288 3900 4948 5700 6624 7132 7923 9444 10935 12428 14008 15438]; % detected recovered
+N = 67064000; % population in FR
+J = length(I0);
+T1 = 4; % the date of quarantine
+T0 = datetime(2020,3,12); % the initial date for data
+
+if (Imperial == 1) % Imperial
+    alpha1 = 53; alpha2 = alpha1;
+    % Confinement parameters for France
+    pC = 1.5; % number of contacts in quarantine
+    pN = 4; % number of contacts in normal state 
+    pR = 2.75; % number of contacts in relaxed quarantine
+elseif (Imperial == 2) % Cmmdi
+     alpha1 = 15; alpha2 = alpha1;
+end
+
+disp(' ');
+disp([' ***' Country '*** ']);
+disp(' ');
+
+script1(delta,0.5*delta,China);
+input('Press ENTER to proceed, please')
+close all;
+
+%% Italy
+Country = 'Italy';
+
+% Statistics from 05/03 (eficiens + hopkins)
+I0 = [3858 4636 5883 7375 9172 10149 12462 15113 17660 21157 24747 27980 31506 35713 41035 47021 53578 59158 63927 69176 74386 80589 86498 92472 97689 101739 105792 110574 115242 119827 124632]; % detected infected
+D0 = [148 197 233 366 463 631 827 1016 1266 1441 1809 2158 2503 2978 3405 4032 4825 5476 6077 6820 7503 8215 9134 10023 10779 11591 12428 13155 13915 14681 15362]; % detected deaths
+H0 = [414 523 589 622 724 1004 1045 1258 1439 1439 1439 2749 2941 4025 4440 5129 6072 7024 7432 8326 9362 10361 10950 12384 13030 14620 15729 16847 18278 19388 20996]; % detected recovered
+N = 60359546; % population in IT
+J = length(I0);
+T1 = 2; % the date of quarantine
+T0 = datetime(2020,3,05); % the initial date for data
+
+if (Imperial == 1) % Imperial
+    alpha1 = 64; alpha2 = alpha1;
+    % Confinement parameters for Italy
+    pC = 1.5; % number of contacts in quarantine
+    pN = 3.8; % number of contacts in normal state 
+    pR = 2.7; % number of contacts in relaxed quarantine
+elseif (Imperial == 2) % Cmmdi
+    alpha1 = 17; alpha2 = alpha1;
+end
+
+disp(' ');
+disp([' ***' Country '*** ']);
+disp(' ');
+
+script1(delta,delta,China);
+input('Press ENTER to proceed, please')
+close all;
+
+%% Spain
+Country = 'Spain';
+
+% Statistics from 12/03 (https://es.statista.com/estadisticas/1104279/personas-con-covid-19-recuperadas-por-dia-espana/ + Hopkins)
+I0 = [2950 4209 5753 7753 9383 11273 13716 17147 19980 24926 28572 33089 39675 47600 56188 64059 73235 80110 87956 95923 104118 112065 119199 126168]; % detected infected
+D0 = [84 118 136 288 309 497 598 767 982 1326 1720 2182 2696 3434 4365 5138 5982 6803 7716 8464 9387 10348 11198 11947]; % detected deaths
+H0 = [189 193 517 517 530 1028 1081 1107 1588 2125 2575 3355 3794 5367 7015 9357 12285 14709 16780 19259 22647 26743 30513 34219]; % detected recovered
+N = 46600396; % population in ES
+J = length(I0);
+T1 = 2; % the date of quarantine
+T0 = datetime(2020,3,12); % the initial date for data
+
+if (Imperial == 1) % Imperial
+    alpha1 = 95; alpha2 = alpha1;
+    % Confinement parameters for Spain
+    pC = 1.7; % number of contacts in quarantine
+    pN = 4.8; % number of contacts in normal state 
+    pR = 3.2; % number of contacts in relaxed quarantine
+elseif (Imperial == 2) % Cmmdi
+     alpha1 = 18.5; alpha2 = alpha1;
+end
+
+disp(' ');
+disp([' ***' Country '*** ']);
+disp(' ');
+
+script1(0.5*delta,delta,China);
+input('Press ENTER to proceed, please')
+close all;
+
+%% Germany
+Country = 'Germany';
+
+% Statistics from 12/03 (Hopkins)
+I0 = [2369 3062 3795 4838 6012 7156 8198 10999 13957 16662 18610 22672 27436 31554 36508 42288 48582 52547 57298 61913 67366 77981 84794 91159 96092]; % detected infected
+D0 = [5 5 8 12 12 12 12 20 31 47 55 86 114 149 198 253 325 389 455 583 775 931 1107 1275 1444]; % detected deaths
+H0 = [25 25 46 46 46 67 67 105 115 180 209 266 453 3290 3547 5673 6658 8481 9211 13500 16100 18700 22440 24575 26400]; % detected recovered
+N = 83042200; % population in GE (https://en.wikipedia.org/wiki/Demographics_of_Germany)
+J = length(I0);
+T1 = 10; % the date of quarantine
+T0 = datetime(2020,3,12); % the initial date for data
+
+if (Imperial == 1) % Imperial
+    alpha1 = 8; alpha2 = alpha1;
+    % Confinement parameters for Germany
+    pC = 1.8; % number of contacts in quarantine
+    pN = 4.6; % number of contacts in normal state 
+    pR = 3.2; % number of contacts in relaxed quarantine
+elseif (Imperial == 2) % Cmmdi
+     alpha1 = 2.13; alpha2 = alpha1;
+end
+
+disp(' ');
+disp([' ***' Country '*** ']);
+disp(' ');
+
+script1(2*delta,0.5*delta,China);
+input('Press ENTER to proceed, please')
+close all;
+
+%% Brazil
+Country = 'Brazil';
+
+% Statistics from 12/03 (hopkins)
+I0 = [77 151 151 200 234 346 529 640 970 1178 1546 1924 2247 2554 2985 3477 3904 4256 4661 5812 6931 8066 9216 10360]; % detected infected
+D0 = [0 0 0 0 0 1 4 7 11 18 25 34 46 59 77 93 114 136 165 202 244 327 365 445]; % detected deaths
+H0 = [0 1 1 1 2 2 2 2 2 2 2 2 2 2 6 6 6 6 127 127 127 127 127 127]; % detected recovered
+N = 212559417; % population in BR
+J = length(I0);
+T1 = 2; % the date of quarantine
+T0 = datetime(2020,3,12); % the initial date for data
+
+if (Imperial == 1) % No Imperial
+    alpha1 = 5; alpha2 = alpha1;
+    % Confinement parameters for Brazil
+    pC = 2; % number of contacts in quarantine
+    pN = 12; % number of contacts in normal state 
+    pR = 6; % number of contacts in relaxed quarantine
+elseif (Imperial == 2) % Cmmdi
+     alpha1 = 9; alpha2 = alpha1;
+end
+
+disp(' ');
+disp([' ***' Country '*** ']);
+disp(' ');
+
+script1(delta,delta,China);
+input('Press ENTER to proceed, please')
+close all;
+
+%% Russia
+Country = 'Russia';
+
+% Statistics from 12/03 (Wikipedia + hopkins)
+I0 = [45 59 63 93 114 147 199 253 306 367 438 495 658 840 1036 1264 1534 1836 2337 2777 3548 4149 4731 4874]; % detected infected
+D0 = [0 0 0 0 0 0 0 0 0 0 0 0 0 2 3 4 8 9 17 24 30 34 43 45]; % detected deaths
+H0 = [3 3 3 4 5 5 5 12 16 16 17 22 29 38 45 49 64 66 121 190 235 281 333 355]; % detected recovered
+N = 146745098; % population in RU
+J = length(I0);
+T1 = 15; % the date of quarantine
+T0 = datetime(2020,3,12); % the initial date for data
+
+if (Imperial == 1) % No Imperial
+    alpha1 = 5; alpha2 = alpha1;
+    % Confinement parameters for Russia
+    pC = 2; % number of contacts in quarantine
+    pN = 12; % number of contacts in normal state 
+    pR = 6; % number of contacts in relaxed quarantine
+elseif (Imperial == 2) % Cmmdi
+    alpha1 = 2.5; alpha2 = alpha1;
+end
+
+disp(' ');
+disp([' ***' Country '*** ']);
+disp(' ');
+
+script1(delta,delta,China);
+disp('Stop execution')
---
+++ script1.m
@@ -0,0 +1,284 @@
+function script1(deltap,deltam,China)
+
+global sigma gamma b alpha1 alpha2 Mort_Rate pC pN pR wC wR NC scenario scenario_p JDI JAI K Pos Posl N J T1 T0 Country I0 D0 H0 delay Type J_id;
+
+%% Estimation of alpha
+Ka = J-JAI-JDI; A = zeros(Ka,1);
+for i = 0:Ka-1
+    A(i+1) = (D0(J-i)-D0(J-i-JDI))/(I0(J-i-JDI)-I0(J-i-JDI-JAI+1))/Mort_Rate;
+end
+alpha = mean(A)
+figure('Name',['Alpha for ' Country],'Position',Pos);
+plot(1:Ka,A,'b',1:Ka,alpha*ones(Ka,1),'b:');
+I0 = alpha1*(I0-D0-H0); R0 = D0+alpha2*H0; % measured recovered 
+
+%% Schedule of quarantine
+M = T1+7*(wC+wR)*NC; % number of days to simulate
+t = T0 + caldays(0:T1+7*(wC+wR)*NC-1);
+r = pC*ones(M,1); r(1:T1) = pN;
+r(T1+wC*7+1:T1+(wC+wR)*7) = pR;
+ticks = [t(T1+1) t(T1+wC*7) t(T1+(wC+wR)*7)];
+for i = 2:NC
+    r(T1+(i-1)*(wC+wR)*7+wC*7+1:T1+i*(wC+wR)*7) = pR;
+    ticks = [ticks t(T1+(i-1)*(wC+wR)*7+wC*7) t(T1+i*(wC+wR)*7)];
+end
+figure('Name',['Number of contacts for ' Country],'Position',Pos);
+plot(t,r); ylim([pC-1 pN+1]);
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+
+%% Simulation for Chinese data
+if (China == 1)
+b_ = mean(b); gamma_ = mean(gamma);
+R = ones(M,1); R(1) = R0(1);
+I = ones(M,1); I(1) = I0(1);
+E = ones(M,1); E(1) = (I0(2)-(1-gamma_)*I0(1))/sigma; % alpha*I(1);
+S = ones(M,1); S(1) = N-R(1)-I(1)-E(1);
+
+for i = 1:M-1
+    S(i+1) = S(i)-b_*(pC*I(i)+r(delay_ind(i-delay))*E(i))*S(i)/N;
+    E(i+1) = (1-sigma)*E(i)+b_*(pC*I(i)+r(delay_ind(i-delay))*E(i))*S(i)/N;
+    I(i+1) = (1-gamma_)*I(i)+sigma*E(i);
+    R(i+1) = R(i)+gamma_*I(i);
+end
+
+figure('Name',['Simulation with Chinese data for scenario ' num2str(scenario) ' in ' Country],'Position',Pos);
+semilogy(t,E,'r',t,I,'b',t,R,'g',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); legend('E','I','R','Location','southeast');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+
+figure('Name',['Verification of Chinese parameters by measured data for ' Country],'Position',Pos);
+subplot(2,1,1); semilogy(t(1:J),I(1:J),'b',t(1:J),I0,'b--'); title('I'); xlim([t(1) t(J)]);
+subplot(2,1,2); semilogy(t(1:J),R(1:J),'g',t(1:J),R0,'g--'); title('R'); xlim([t(1) t(J)]);
+end
+
+%% Estimation of parameters
+dI0 = log(I0(2:J-J_id))-log(I0(1:J-J_id-1));
+if (J-J_id-2-T1 > 0)
+    E_tau = zeros(J-J_id-2-T1,1);
+    for i = T1+1:J-J_id-1
+        E_tau(i-T1) = norm(dI0-[mean(dI0(1:i-1))*ones(1,i-1) mean(dI0(i:J-J_id-1))*ones(1,J-J_id-i)]);
+    end
+    [y, i_min] = min(E_tau); i_min = max(i_min);
+    tau = min(delay,i_min)
+    tau = delay;
+else
+    tau = delay;
+end
+if tau == delay
+    i_min = min(J-J_id-1,T1+delay);
+end
+if (scenario_p == 1)
+    B = zeros(J-J_id-K,1); G = zeros(J-J_id-K,1);
+    for k = 0:J-J_id-1-K
+        den = 0; num = 0;
+        for i = 1:K
+            den = den+I0(k+i)^2;
+            num = num+(R0(k+i+1)-R0(k+i))*I0(k+i);
+        end
+        G(k+1) = num/den;
+    end
+    gamma_i = mean(G)
+    E0 = ones(J-J_id-1,1); S0 = ones(J-J_id-1,1);
+    for i = 2:J-J_id
+        E0(i-1) = (I0(i)-(1-gamma_i)*I0(i-1))/sigma;
+        if E0(i-1) < 0
+            if i > 2
+                E0(i-1) = E0(i-2);
+            else
+                E0(i-1) = 1;
+            end
+        end
+        S0(i-1) = N-R0(i-1)-I0(i-1)-E0(i-1);
+    end
+    for k = 0:J-J_id-1-K
+        den = 0; num = 0;
+        for i = 2:K
+            den = den+(S0(k+i-1)*(pC*I0(k+i-1)+r(delay_ind(k+i-1-tau))*E0(k+i-1)))^2;
+            num = num+(E0(k+i)-(1-sigma)*E0(k+i-1))*S0(k+i-1)*(pC*I0(k+i-1)+r(delay_ind(k+i-1-tau))*E0(k+i-1));
+        end
+        B(k+1) = N*num/den;
+    end
+    b_i = mean(B(B>0))
+    figure('Name',['Identified parameters on a window for ' Country],'Position',Pos);
+    subplot(3,1,1); plot(t(K+1:J-J_id),G,'m',t(K+1:J-J_id),gamma_i*ones(J-J_id-K,1),'m:'); title('\gamma'); xlim([t(K+1) t(J-J_id)]);
+    subplot(3,1,2); plot(t(K+1:J-J_id),B,'r',t(K+1:J-J_id),b_i*ones(J-J_id-K,1),'r:'); title('b'); xlim([t(K+1) t(J-J_id)]);
+    subplot(3,1,3); plot(t(2:J-J_id),dI0,'c',t(2:J-J_id),[mean(dI0(1:i_min-1))*ones(1,i_min-1) mean(dI0(i_min:J-J_id-1))*ones(1,J-J_id-i_min)],'c--'); title('\tau'); xlim([t(2) t(J-J_id)]);
+else
+    Ki = J-J_id-K; % number of days starting from which estimate the values of parameters
+    B = zeros(Ki+1,1); G = zeros(Ki+1,1);
+    for k = Ki:-1:0
+        den = 0; num = 0;
+        for i = 1:J-J_id-k-1
+            den = den+I0(i)^2;
+            num = num+(R0(i+1)-R0(i))*I0(i);
+        end
+        G(Ki-k+1) = num/den;
+    end
+    gamma_i = mean(G)
+    E0 = ones(J-J_id-1,1); S0 = ones(J-J_id-1,1);
+    for i = 2:J-J_id
+        E0(i-1) = (I0(i)-(1-gamma_i)*I0(i-1))/sigma;
+        if E0(i-1) < 0
+            if i > 2
+                E0(i-1) = E0(i-2);
+            else
+                E0(i-1) = 1;
+            end
+        end
+        S0(i-1) = N-R0(i-1)-I0(i-1)-E0(i-1);
+    end
+    for k = Ki:-1:0
+        den = 0; num = 0;
+        for i = 2:J-J_id-k-1
+            den = den+(S0(i-1)*(pC*I0(i-1)+r(delay_ind(i-1-tau))*E0(i-1)))^2;
+            num = num+(E0(i)-(1-sigma)*E0(i-1))*S0(i-1)*(pC*I0(i-1)+r(delay_ind(i-1-tau))*E0(i-1));
+        end
+        B(Ki-k+1) = N*num/den;
+    end
+    b_i = mean(B(B>0))
+    figure('Name',['Identified parameters for ' Country],'Position',Pos);
+    subplot(3,1,1); plot(t(J-J_id-Ki-1:J-J_id-1),G,'m',t(J-J_id-Ki-1:J-J_id-1),gamma_i*ones(Ki+1,1),'m:'); title('\gamma'); xlim([t(J-J_id-Ki-1) t(J-J_id-1)]);
+    subplot(3,1,2); plot(t(J-J_id-Ki-1:J-J_id-1),B,'r',t(J-J_id-Ki-1:J-J_id-1),b_i*ones(Ki+1,1),'r:'); title('b'); xlim([t(J-J_id-Ki-1) t(J-J_id-1)]);
+    subplot(3,1,3); plot(t(2:J-J_id),dI0,'c',t(2:J-J_id),[mean(dI0(1:i_min-1))*ones(1,i_min-1) mean(dI0(i_min:J-J_id-1))*ones(1,J-J_id-i_min)],'c--'); title('\tau'); xlim([t(2) t(J-J_id)]);
+end
+
+%% Simulation for identified data
+R(1) = R0(1); I(1) = I0(1); E(1) = E0(1); S(1) = S0(1);
+
+for i = 1:M-1
+    S(i+1) = S(i)-b_i*(pC*I(i)+r(delay_ind(i-tau))*E(i))*S(i)/N;
+    E(i+1) = (1-sigma)*E(i)+b_i*(pC*I(i)+r(delay_ind(i-tau))*E(i))*S(i)/N;
+    I(i+1) = (1-gamma_i)*I(i)+sigma*E(i);
+    R(i+1) = R(i)+gamma_i*I(i);
+end
+
+figure('Name',['Simulation with identified data for scenario ' num2str(scenario) ' in ' Country],'Position',Pos);
+semilogy(t,E,'r',t,I,'b',t,R,'g',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); legend('E','I','R','Location','southeast');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+
+figure('Name',['Verification of identified parameters by measured data for ' Country],'Position',Pos);
+subplot(2,2,1); semilogy(t(1:J),I(1:J),'b',t(1:J-J_id),I0(1:J-J_id),'b--',t(J-J_id+1:J),I0(J-J_id+1:J),'bs'); title('I'); xlim([t(1) t(J)]);
+subplot(2,2,2); semilogy(t(1:J),R(1:J),'g',t(1:J-J_id),R0(1:J-J_id),'g--',t(J-J_id+1:J),R0(J-J_id+1:J),'gs'); title('R'); xlim([t(1) t(J)]);
+subplot(2,2,3); semilogy(t(1:J-J_id-1),E(1:J-J_id-1),'r',t(1:J-J_id-1),E0,'r--'); title('E'); xlim([t(1) t(J-J_id-1)]);
+subplot(2,2,4); semilogy(t(1:J-J_id-1),S(1:J-J_id-1),'m',t(1:J-J_id-1),S0,'m--'); title('S'); xlim([t(1) t(J-J_id-1)]);
+
+%% Interval observer simulaiton for deviation of all parameters
+delta_ = [1-deltam 1+deltap]; % variation for intervals
+gamma_ = gamma_i*delta_; b_ = b_i*delta_; r_ = ceil(r.*delta_); sigma_ = sigma*delta_;
+Rm = zeros(M,1); RM = zeros(M,1); Im = zeros(M,1); IM = zeros(M,1);
+Em = zeros(M,1); EM = zeros(M,1); Sm = zeros(M,1); SM = zeros(M,1);
+% inirial conditions
+Rm(1) = delta_(1)*R(1); RM(1) = delta_(2)*R(1);
+Im(1) = delta_(1)*I(1); IM(1) = delta_(2)*I(1);
+Em(1) = delta_(1)*E(1); EM(1) = delta_(2)*E(1);
+Sm(1) = delta_(1)*S(1); SM(1) = delta_(2)*S(1);
+
+for i = 1:M-1
+    Sm(i+1) = (1-b_(2)*(pC*IM(i)+r_(delay_ind(i-tau),2)*EM(i))/N)*Sm(i);
+    Em(i+1) = (1-sigma_(2)+b_(1)*r_(delay_ind(i-tau),1)*Sm(i)/N)*Em(i)+b_(1)*pC*Im(i)*Sm(i)/N;
+    Im(i+1) = (1-gamma_(2))*Im(i)+sigma_(1)*Em(i);
+    Rm(i+1) = Rm(i)+gamma_(1)*Im(i);
+    SM(i+1) = min(N,(1-b_(1)*(pC*Im(i)+r_(delay_ind(i-tau),1)*Em(i))/N)*SM(i));
+    EM(i+1) = min(N,(1-sigma_(1)+b_(2)*r_(delay_ind(i-tau),2)*SM(i)/N)*EM(i)+b_(2)*pC*IM(i)*SM(i)/N);
+    IM(i+1) = min(N,(1-gamma_(1))*IM(i)+sigma_(2)*EM(i));
+    RM(i+1) = min(N,RM(i)+gamma_(2)*IM(i));
+end
+
+figure('Name',['Simulation of interval predictor for deviation of all parameters for ' Country],'Position',Posl);
+subplot(2,2,1); semilogy(t,E,'r',t,Em,'r:',t,EM,'r--',t(1:J-J_id-1),E0,'ro',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); title('E');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+subplot(2,2,2); semilogy(t,I,'b',t,Im,'b:',t,IM,'b--',t(1:J-J_id),I0(1:J-J_id),'bo',t(J-J_id+1:J),I0(J-J_id+1:J),'bs',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); title('I');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+subplot(2,2,3); semilogy(t,R,'g',t,Rm,'g:',t,RM,'g--',t(1:J-J_id),R0(1:J-J_id),'go',t(J-J_id+1:J),R0(J-J_id+1:J),'gs',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); title('R');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+subplot(2,2,4); semilogy(t,S,'m',t,Sm,'m:',t,SM,'m--',t(1:J-J_id-1),S0,'mo',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); title('S');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+
+figure('Name',['Interval predictor of infectives for deviation of all parameters for ' Country],'Position',Pos);
+if (Type == 1)
+    semilogy(t,Im,'b:',t,IM,'b--',t(1:J-J_id),I0(1:J-J_id),'bo',t(J-J_id+1:J),I0(J-J_id+1:J),'bs',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); title('I');
+else
+    plot(t(1:J-J_id),I0(1:J-J_id),'bo',t(J-J_id+1:J),I0(J-J_id+1:J),'bs'); xlim([t(1) t(M)]); title('I'); %,t,N*ones(M,1),'k:'
+end
+ax = gca; set(ax,'xTick',ticks,'XGrid','on'); hold on
+
+% initial conditions from the last date used for identification
+Rm(J-J_id-1) = delta_(1)*R0(J-J_id-1); RM(J-J_id-1) = delta_(2)*R0(J-J_id-1);
+Im(J-J_id-1) = delta_(1)*I0(J-J_id-1); IM(J-J_id-1) = delta_(2)*I0(J-J_id-1);
+Em(J-J_id-1) = delta_(1)*E0(J-J_id-1); EM(J-J_id-1) = delta_(2)*E0(J-J_id-1);
+Sm(J-J_id-1) = delta_(1)*S0(J-J_id-1); SM(J-J_id-1) = delta_(2)*S0(J-J_id-1);
+R_ = zeros(M,1); E_ = zeros(M,1); I_ = zeros(M,1); S_ = zeros(M,1);
+E_(J-J_id-1) = E0(J-J_id-1); R_(J-J_id-1) = R0(J-J_id-1); I_(J-J_id-1) = I0(J-J_id-1); S_(J-J_id-1) = S0(J-J_id-1);
+
+for i = J-J_id-1:M-1
+    S_(i+1) = S_(i)-b_i*(pC*I_(i)+r(delay_ind(i-tau))*E_(i))*S_(i)/N;
+    E_(i+1) = (1-sigma)*E_(i)+b_i*(pC*I_(i)+r(delay_ind(i-tau))*E_(i))*S_(i)/N;
+    I_(i+1) = (1-gamma_i)*I_(i)+sigma*E_(i);
+    R_(i+1) = R_(i)+gamma_i*I_(i);
+    Sm(i+1) = (1-b_(2)*(pC*IM(i)+r_(delay_ind(i-tau),2)*EM(i))/N)*Sm(i);
+    Em(i+1) = (1-sigma_(2)+b_(1)*r_(delay_ind(i-tau),1)*Sm(i)/N)*Em(i)+b_(1)*pC*Im(i)*Sm(i)/N;
+    Im(i+1) = (1-gamma_(2))*Im(i)+sigma_(1)*Em(i);
+    Rm(i+1) = Rm(i)+gamma_(1)*Im(i);
+    SM(i+1) = min(N,(1-b_(1)*(pC*Im(i)+r_(delay_ind(i-tau),1)*Em(i))/N)*SM(i));
+    EM(i+1) = min(N,(1-sigma_(1)+b_(2)*r_(delay_ind(i-tau),2)*SM(i)/N)*EM(i)+b_(2)*pC*IM(i)*SM(i)/N);
+    IM(i+1) = min(N,(1-gamma_(1))*IM(i)+sigma_(2)*EM(i));
+    RM(i+1) = min(N,RM(i)+gamma_(2)*IM(i));
+end
+
+if (Type == 1)
+    semilogy(t(J-J_id-1:M),I_(J-J_id-1:M),'r',t(J-J_id-1:M),Im(J-J_id-1:M),'b:',t(J-J_id-1:M),IM(J-J_id-1:M),'b--','LineWidth',2);
+else
+    plot(t(J-J_id-1:M),I_(J-J_id-1:M),'r',t(J-J_id-1:M),Im(J-J_id-1:M),'b:','LineWidth',2); %,t(J-J_id-1:M),IM(J-J_id-1:M),'b--'
+end
+[y,x] = max(I_(J-J_id-1:M)); 
+if (Type == 1)
+    s = 1.6*y;
+else
+    s = 1.05*y;
+end
+if (x < M-J-J_id+2)
+    x = t(x+J-J_id-2);
+    line('XData', [x x t(1)],'YData',[1 y y],'LineWidth',1,'LineStyle', ':','Color','r');
+    text(x, s, ['I_{peak} = ' num2str(floor(y))]);
+end
+[y,x] = max(Im(J-J_id-1:M));
+if (Type == 1)
+    s = 1.7*y;
+else
+    s = 1.1*y;
+end
+if (x < M-J-J_id+2)
+    x = t(x+J-J_id-2);
+    line('XData', [x x t(1)],'YData',[1 y y],'LineWidth',1,'LineStyle', ':','Color','r');
+    text(x, s, ['I_{peak}^{optimiste} = ' num2str(floor(y))]);
+end
+
+%% Interval observer simulation for deviation of sigma
+gamma_ = gamma_i*[1 1]; b_ = b_i*[1 1]; r_ = ceil(r.*[1 1]); sigma_ = [1/12 1/2];
+Rm = zeros(M,1); RM = zeros(M,1); Im = zeros(M,1); IM = zeros(M,1);
+Em = zeros(M,1); EM = zeros(M,1); Sm = zeros(M,1); SM = zeros(M,1);
+% inirial conditions
+Rm(1) = delta_(1)*R(1); RM(1) = delta_(2)*R(1);
+Im(1) = delta_(1)*I(1); IM(1) = delta_(2)*I(1);
+Em(1) = delta_(1)*E(1); EM(1) = delta_(2)*E(1);
+Sm(1) = delta_(1)*S(1); SM(1) = delta_(2)*S(1);
+
+for i = 1:M-1
+    Sm(i+1) = (1-b_(2)*(pC*IM(i)+r_(delay_ind(i-tau),2)*EM(i))/N)*Sm(i);
+    Em(i+1) = (1-sigma_(2)+b_(1)*r_(delay_ind(i-tau),1)*Sm(i)/N)*Em(i)+b_(1)*pC*Im(i)*Sm(i)/N;
+    Im(i+1) = (1-gamma_(2))*Im(i)+sigma_(1)*Em(i);
+    Rm(i+1) = Rm(i)+gamma_(1)*Im(i);
+    SM(i+1) = min(N,(1-b_(1)*(pC*Im(i)+r_(delay_ind(i-tau),1)*Em(i))/N)*SM(i));
+    EM(i+1) = min(N,(1-sigma_(1)+b_(2)*r_(delay_ind(i-tau),2)*SM(i)/N)*EM(i)+b_(2)*pC*IM(i)*SM(i)/N);
+    IM(i+1) = min(N,(1-gamma_(1))*IM(i)+sigma_(2)*EM(i));
+    RM(i+1) = min(N,RM(i)+gamma_(2)*IM(i));
+end
+
+figure('Name',['Simulation of interval predictor for deviation of sigma for ' Country],'Position',Posl);
+subplot(2,2,1); semilogy(t,E,'r',t,Em,'r:',t,EM,'r--',t(1:J-J_id-1),E0,'ro',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); title('E');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+subplot(2,2,2); semilogy(t,I,'b',t,Im,'b:',t,IM,'b--',t(1:J-J_id),I0(1:J-J_id),'bo',t(J-J_id+1:J),I0(J-J_id+1:J),'bs',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); title('I');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+subplot(2,2,3); semilogy(t,R,'g',t,Rm,'g:',t,RM,'g--',t(1:J-J_id),R0(1:J-J_id),'go',t(J-J_id+1:J),R0(J-J_id+1:J),'gs',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); title('R');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
+subplot(2,2,4); semilogy(t,S,'m',t,Sm,'m:',t,SM,'m--',t(1:J-J_id-1),S0,'mo',t,N*ones(M,1),'k:'); xlim([t(1) t(M)]); title('S');
+ax = gca; set(ax,'xTick',ticks,'XGrid','on');
